ab87709150490cc31d67d9bceaf9f843546c2017
AMI <a_yamamoto@innovation.co.jp>
2018-01-20 22:03:21 +0900

    タイマーを表示させるラベルをコードに紐付け

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //時間を表示させるラベル
+    @IBOutlet weak var timeLabel: UILabel!
+    
     //画像のインスタンスを作る
     var topImage:UIImage?
     var lastImage:UIImage?
@@ -18,10 +22,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var timer = Timer.sc
         
         //画像を表示させる
-//        imageView.image =
+        topImage = UIImage(named: "neko")
+        displayImg(img: topImage!)
+    }
+    
+    //以下はいらないかも
+    //画像のサイズを取得して、適正な縮尺で表示させる
+    func displayImg(img:UIImage){
         
+        //画面サイズを取得する
+        let screenWidth = self.view.bounds.width
+        let screenHeight = self.view.bounds.height
+        
+        //画像のサイズを取得する
+        let imgWidth = img.size.width
+        let imgHeight = img.size.height
+        
+        //画像が画面横幅7割になるよう縮尺を取得する
+        let scare = (screenWidth / imgWidth) * 0.7
+        
+        //画像サイズを変更する
+        let rect = CGRect(x: 0, y: 0, width:  imgWidth * scare, height: imgHeight * scare)
+        imageView.frame = rect
+        
+        //画像位置を変更する
+        imageView.center = CGPoint(x: screenWidth/2, y: screenHeight * 0.6)
+        
+        //画像を表示させる
+        imageView.image = img
     }
 
     override func didReceiveMemoryWarning() {
